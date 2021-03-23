cba95d8b05124e48d6c16b9a25d709e0b2cf107b
Vadym Zelinskyi <vadym.zelinskyi@rubyplay.com>
2021-03-23 20:23:14 +0200

    Added timer for alert

--- game-client/src/App.jsx
+++ game-client/src/App.jsx
@@ -1,37 +1,48 @@
 import { useDispatch, useSelector } from 'react-redux';
-import React, { useEffect } from 'react';
-import history from './helpers/history';
+import React, { useEffect, useState } from 'react';
 import alertActions from './redux/actions/AlertActions';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Registration from './pages/Registration'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 export default function App() {
-    const alert = useSelector(state => state.alert);
-    const dispatch = useDispatch();
+  const [showAlert, setShowAlert] = useState(false);
+  const alert = useSelector(state => state.alert);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    useEffect(() => history.listen(() => {
-        dispatch(alertActions.clear());
-    }), []);
+  useEffect(() => history.listen((data) => {
+    console.log(data);
+    dispatch(alertActions.clear());
+  }), []);
 
-    return (
-        <>
-            <Header/>
-            <div className='jumbotron'>
-                <div className='col-md-8 offset-md-2'>
-                    {alert.message ?
-                        <div className={`alert ${alert.type}`}>{alert.message}</div> : null}
-                    <Switch>
-                        <Route exact path='/' component={Home}/>
-                        <Route path='/login' component={Login}/>
-                        <Route path='/register' component={Registration}/>
-                    </Switch>
-                </div>
-            </div>
-            <Footer/>
-        </>
-    );
+  useEffect(() => {
+    if (alert.message) {
+      const timer = setTimeout(() => setShowAlert(false), 5000);
+
+      setShowAlert(true)
+
+      return () => clearTimeout(timer);
+    }
+  }, [alert.message]);
+
+  return (
+    <>
+      <Header />
+      <div className='jumbotron'>
+        <div className='col-md-8 offset-md-2'>
+          {showAlert && <div className={`alert ${alert.type}`}>{alert.message}</div>}
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Registration} />
+          </Switch>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
